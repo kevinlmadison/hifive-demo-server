f97244f8fc390616f042639f1aa21619c50a03d2
kevinlmadison <coolklm121@gmail.com>
2024-03-04 10:18:53 -0500

    maybe fix cors

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,13 @@ use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
-    // let cors = CorsLayer::new()
-    //     .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
-    //     .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
-    //     .allow_credentials(true)
-    //     .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+    let cors = CorsLayer::new()
+        .allow_origin("http://192.168.1.4:8000/api/command".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
-    // let app = create_router().layer(cors);
-    let app = create_router();
+    let app = create_router().layer(cors);
 
     println!("🚀 Server started successfully");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
