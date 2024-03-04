0410d97730107f1824d2097ed8c468d068183933
kevinlmadison <coolklm121@gmail.com>
2024-03-04 10:58:21 -0500

    cors is fun

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ use tower_http::cors::CorsLayer;
 #[tokio::main]
 async fn main() {
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:8000".parse::<HeaderValue>().unwrap())
-        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
-        .allow_credentials(true)
-        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+        .allow_origin("*".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE]);
+        // .allow_credentials(true)
+        // .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
     let app = create_router().layer(cors);
 
